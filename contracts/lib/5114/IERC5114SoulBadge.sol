a7a9ee0aa386eb2d540331f226a08c89fc8ba306
Jason Hoi <flashjx1985@gmail.com>
2022-09-18 20:19:54 +0800

    emove IERC165 OZ dependency.

--- contracts/lib/5114/IERC5114SoulBadge.sol
+++ contracts/lib/5114/IERC5114SoulBadge.sol
@@ -11,7 +11,6 @@
 
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "./IERC5114.sol";
 
 /**
@@ -25,12 +24,33 @@ interface IERC721Metadata {
 }
 
 /**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+/**
  * @dev See https://eips.ethereum.org/EIPS/eip-5114
  * This is additional interface on top of EIP-5114
  *
  * (bytes4) 0xb9d11845 = type(IERC5114SoulBadge).interfaceId
  */
-interface IERC5114SoulBadge is IERC165, IERC721Metadata, IERC5114 {
+interface IERC5114SoulBadge is IERC5114, IERC165, IERC721Metadata {
   // Returns badge token balance for a `Soul`
   function balanceOfSoul(address soulContract, uint256 soulTokenId) external view returns (uint256);
 
