7e13776e570fa520a7609f97f4f9b8cd58e926d9
Jason Hoi <flashjx1985@gmail.com>
2022-09-18 22:49:03 +0800

    ERC5114 badge contract, create UUPS upgradeable version with test.

---
+++ contracts/sharkz-upgradeable/SharkzSoulBadge-v1.sol
@@ -0,0 +1,255 @@
+// SPDX-License-Identifier: MIT
+
+/**
+       █                                                                        
+▐█████▄█ ▀████ █████  ▐████    ████████    ███████████  ████▌  ▄████ ███████████
+▐██████ █▄ ▀██ █████  ▐████   ██████████   ████   ████▌ ████▌ ████▀       ████▀ 
+  ▀████ ███▄ ▀ █████▄▄▐████  ████ ▐██████  ████▄▄▄████  █████████        ████▀  
+▐▄  ▀██ █████▄ █████▀▀▐████ ▄████   ██████ █████████    █████████      ▄████    
+▐██▄  █ ██████ █████  ▐█████████▀    ▐█████████ ▀████▄  █████ ▀███▄   █████     
+▐████  █▀█████ █████  ▐████████▀        ███████   █████ █████   ████ ███████████
+       █
+ *******************************************************************************
+ * Sharkz Soul Badge
+ *******************************************************************************
+ * Creator: Sharkz Entertainment
+ * Author: Jason Hoi
+ *
+ */
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "../lib/sharkz/IScore.sol";
+import "../lib-upgradeable/sharkz/AdminableUpgradeable.sol";
+import "../lib-upgradeable/5114/ERC5114SoulBadgeUpgradeable.sol";
+import "../lib-upgradeable/712/EIP712WhitelistUpgradeable.sol";
+
+interface IBalanceOf {
+    function balanceOf(address owner) external view returns (uint256 balance);
+}
+
+interface IVoter {
+    // Get voter vote value for a poll
+    function getAddressVote(uint256 _pid, address _addr) external view returns (uint256);
+}
+
+contract SharkzSoulBadgeV1 is IScore, Initializable, UUPSUpgradeable, AdminableUpgradeable, ReentrancyGuardUpgradeable, EIP712WhitelistUpgradeable, ERC5114SoulBadgeUpgradeable {
+    // Implementation version number
+    function version() external pure virtual returns (string memory) { return "1.1"; }
+
+    // Keep track of total minted token count
+    uint256 public tokenMinted;
+
+    // Mint modes, 0: disable-minting, 1: free-mint, 2: restrict minting to target token owner, 3: restrict to voter
+    uint256 public mintMode;
+
+    // Max mint supply
+    uint256 public mintSupply;
+    
+    // Target token contract for limited minting
+    address public tokenContract;
+
+    // Target voting contract for limited minting
+    address public voteContract;
+
+    // Target voting poll Id for limited minting
+    uint256 public votePollId;
+
+    // Minting by claim contract
+    address internal _claimContract;
+
+    // Token image (all token use same image)
+    string public tokenImageUri;
+
+    // Init this upgradeable contract
+    function initialize(string memory _name, string memory _symbol, string memory _collectionUri, string memory _tokenImageUri) public initializer onlyProxy {
+        __Adminable_init();
+        __ReentrancyGuard_init();
+        __EIP712Whitelist_init();
+        __ERC5114SoulBadgeUpgradeable_init(_name, _symbol, _collectionUri, "");
+        // token image is a immuntable fixed uri for all tokens
+        tokenImageUri = _tokenImageUri;
+        // default mint supply 10k
+        mintSupply = 10000;
+    }
+
+    // only admins can upgrade the contract
+    function _authorizeUpgrade(address newImplementation) internal override onlyAdmin {}
+
+    /**
+     * @dev {IERC5114-tokenUri} alias to tokenURI(), so we just override tokenURI()
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "Non-existent token");
+
+        string memory output = string(abi.encodePacked(
+          '{"name":"', name, ' #', _toString(tokenId), '","image":"', tokenImageUri, '"}'
+        ));
+        return string(abi.encodePacked("data:application/json;utf8,", output));
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return super.supportsInterface(interfaceId) || 
+               interfaceId == type(IScore).interfaceId;
+    }
+
+    /**
+     * @dev See {IScore-baseScore}.
+     */
+    function baseScore() public pure virtual override returns (uint256) {
+        return 1;
+    }
+
+    /**
+     * @dev See {IScore-scoreByToken}.
+     */
+    function scoreByToken(uint256 _tokenId) external view virtual override returns (uint256) {
+        if (_exists(_tokenId)) {
+          return 1;
+        } else {
+          return 0;
+        }
+    }
+
+    /**
+     * @dev See {IScore-scoreByAddress}.
+     */
+    function scoreByAddress(address _addr) external view virtual override returns (uint256) {
+        require(_addr != address(0), "Address is the zero address");
+        revert("score by address not supported");
+    }
+
+    // Caller must not be an wallet account
+    modifier callerIsUser() {
+        require(tx.origin == msg.sender, "Caller should not be a contract");
+        _;
+    }
+
+    // Caller must be `Soul` token owner
+    modifier callerIsSoulOwner(address soulContract, uint256 soulTokenId) {
+        require(soulContract != address(0), "Soul contract is the zero address");
+        require(msg.sender == _getSoulOwnerAddress(soulContract, soulTokenId), "Caller is not Soul token owner");
+        _;
+    }
+
+    // Change minting mode
+    function setMintMode(uint256 _mode) external virtual onlyAdmin {
+        mintMode = _mode;
+    }
+
+    // Change mint supply
+    function setMintSupply(uint256 _max) external virtual onlyAdmin {
+        mintSupply = _max;
+    }
+
+    // Update linking IBalanceOf contract address
+    function setMintRestrictContract(address _addr) external onlyAdmin {
+        tokenContract = _addr;
+    }
+
+    // Update linking vote contract and poll Id
+    function setMintRestrictVote(address _addr, uint256 _pid) external onlyAdmin {
+        voteContract = _addr;
+        votePollId = _pid;
+    }
+
+    // Update linking claim contract
+    function setClaimContract(address _addr) external onlyAdmin {
+        _claimContract = _addr;
+    }
+
+    // Returns total valid token count
+    function totalSupply() public view returns (uint256) {
+        return tokenMinted;
+    }
+
+    // Create a new token for Soul
+    function _runMint(address soulContract, uint256 soulTokenId) 
+        private 
+        nonReentrant 
+        onlyProxy
+    {
+        require(mintMode > 0, 'Minting disabled');
+        require(tokenMinted < mintSupply, 'Max minting supply reached');
+
+        // mint to Soul contract and Soul tokenId
+        _mint(tokenMinted, soulContract, soulTokenId);
+        unchecked {
+          tokenMinted += 1;
+        }
+    }
+
+    // Minting by admin to any address
+    function ownerMint(address soulContract, uint256 soulTokenId) 
+        external 
+        onlyAdmin 
+    {
+        _runMint(soulContract, soulTokenId);
+    }
+
+    // Minting from claim contract
+    function claimMint(address soulContract, uint256 soulTokenId) external {
+        require(_claimContract != address(0), "Linked claim contract is not set");
+        require(_claimContract == msg.sender, "Caller is not claim contract");
+        _runMint(soulContract, soulTokenId);
+    }
+
+    // Public minting, limited to Soul Token owner
+    function publicMint(address soulContract, uint256 soulTokenId) 
+        external 
+        callerIsUser() 
+        callerIsSoulOwner(soulContract, soulTokenId)
+    {
+        if (mintMode == 2) {
+            // target token owner
+            require(tokenContract != address(0), "Token contract is the zero address");
+            require(_isExternalTokenOwner(tokenContract, msg.sender), "Caller is not target token owner");
+        }
+        if (mintMode == 3) {
+            // target poll voter
+            require(voteContract != address(0), "Vote contract is the zero address");
+            require(isVoter(voteContract, votePollId, msg.sender), "Caller is not voter");
+        }
+        _runMint(soulContract, soulTokenId);
+    }
+
+    // Minting with signature from contract EIP712 signer, limited to Soul Token owner
+    function whitelistMint(bytes calldata _signature, address soulContract, uint256 soulTokenId) 
+        external 
+        checkWhitelist(_signature) 
+        callerIsUser 
+        callerIsSoulOwner(soulContract, soulTokenId)
+    {
+        _runMint(soulContract, soulTokenId);
+    }
+
+    /**
+     * @dev Returns whether an address is NFT owner
+     */
+    function _isExternalTokenOwner(address _contract, address _ownerAddress) internal view returns (bool) {
+        try IBalanceOf(_contract).balanceOf(_ownerAddress) returns (uint256 balance) {
+            return balance > 0;
+        } catch (bytes memory) {
+          // when reverted, just returns...
+          return false;
+        }
+    }
+
+    /**
+     * @dev Returns whether an address is a voter for a poll
+     */
+    function isVoter(address _contract, uint256 _pid, address _addr) public view returns (bool) {
+        try IVoter(_contract).getAddressVote(_pid, _addr) returns (uint256 voteOption) {
+            return voteOption > 0;
+        } catch (bytes memory) {
+          // when reverted, just returns...
+          return false;
+        }
+    }
+}
---
+++ test/soul-badge-upgrade.js
@@ -0,0 +1,228 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Sharkz Soul Badge', function () {
+  let ownerKey;
+  let signerKey;
+  let guestKey;
+  let guestKey2;
+
+  let soulContract;
+  let contract;
+
+  beforeEach(async function () {
+    const accounts = await ethers.getSigners();
+    ownerKey = accounts[0];
+    signerKey = accounts[1];
+    guestKey = accounts[2];
+    guestKey2 = accounts[3];
+
+    // Soul contract for our Soul badge
+    const Soul = await ethers.getContractFactory('SharkzSoulID');
+    soulContract = await Soul.deploy();
+    await soulContract.deployed();
+    // enable anyone to mint
+    await soulContract.setMintMode(1);
+    await soulContract.ownerMint(guestKey.address);
+    await soulContract.ownerMint(ownerKey.address);
+    await soulContract.ownerMint(guestKey2.address);
+
+    // PFP contract
+    const NFT = await ethers.getContractFactory('NFTERC721');
+    nftContract = await NFT.deploy();
+
+    // Soul Badge contract
+    // deploy v1
+    const v1 = await ethers.getContractFactory('SharkzSoulBadgeV1');
+    contract = await upgrades.deployProxy(v1, [
+      'Genesis PFP Minter',
+      'SZGM',
+      'ipfs://collection.json',
+      'ipfs://tokenBaseUri/',
+    ]);
+    await contract.deployed();
+    await contract.setMintMode(1);
+  });
+
+  it('v1.1 -> v2 -> v1.1', async function () {
+    await contract.setAdmin(guestKey.address, true);
+    expect(await contract.version()).to.equal('1.1');
+
+    // upgrade to v2
+    const v2Contract = await ethers.getContractFactory('SharkzSoulBadgeV2');
+    contract = await upgrades.upgradeProxy(contract.address, v2Contract, [
+      'Genesis PFP Minter',
+      'SZGM',
+      'ipfs://collection.json',
+      'ipfs://tokenBaseUri/',
+    ]);
+    expect(await contract.version()).to.equal('2');
+
+    // rewind to v1
+    const v1Contract = await ethers.getContractFactory('SharkzSoulBadgeV1');
+    contract = await upgrades.upgradeProxy(contract.address, v1Contract, [
+      'Genesis PFP Minter',
+      'SZGM',
+      'ipfs://collection.json',
+      'ipfs://tokenBaseUri/',
+    ]);
+    expect(await contract.version()).to.equal('1.1');
+  });
+
+  it('totalSupply() = 0', async function () {
+    expect(await contract.totalSupply()).to.equal(0);
+    await contract.ownerMint(soulContract.address, 0);
+    expect(await contract.totalSupply()).to.equal(1);
+  });
+
+  it('baseScore(), scoreByToken()', async function () {
+    expect(await contract.baseScore()).to.equal(1);
+    expect(await contract.scoreByToken(0)).to.equal(0);
+
+    // mint by Soul #0
+    await contract.ownerMint(soulContract.address, 0);
+    expect(await contract.scoreByToken(0)).to.equal(1);
+
+    // mint by Soul #1
+    expect(await contract.scoreByToken(1)).to.equal(0);
+    await contract.ownerMint(soulContract.address, 1);
+    expect(await contract.scoreByToken(1)).to.equal(1);
+  });
+
+  it('Mint mode 0: disable mint', async function () {
+    // disable mint
+    await contract.setMintMode(0);
+    await expect(
+      contract.ownerMint(soulContract.address, 0)
+    ).to.be.revertedWith('Minting disabled');
+  });
+
+  it('Mint mode 1: public free mint, setMintSupply', async function () {
+    // publc free mint
+    await contract.setMintMode(1);
+    await contract.ownerMint(soulContract.address, 0);
+
+    await contract.setMintSupply(1);
+    await expect(
+      contract.ownerMint(soulContract.address, 0)
+    ).to.be.revertedWith('Max minting supply reached');
+  });
+
+  it('ownerMint(), balanceOfSoul(), soulOwnerOf()', async function () {
+    expect(await contract.balanceOfSoul(soulContract.address, 0)).to.equal(0);
+    expect(await contract.balanceOfSoul(soulContract.address, 1)).to.equal(0);
+    await expect(contract.soulOwnerOf(0)).to.be.revertedWith(
+      'ERC5114SoulBadge: Soul token owner not found'
+    );
+    await expect(contract.soulOwnerOf(1)).to.be.revertedWith(
+      'ERC5114SoulBadge: Soul token owner not found'
+    );
+
+    // mint by Soul #0
+    await contract.ownerMint(soulContract.address, 0);
+    await expect(
+      contract.ownerMint(soulContract.address, 0)
+    ).to.be.revertedWith('ERC5114SoulBadge: max minting per soul reached');
+
+    // mint by Soul #1
+    await contract.ownerMint(soulContract.address, 1);
+
+    expect(await contract.balanceOfSoul(soulContract.address, 0)).to.equal(1);
+    expect(await contract.balanceOfSoul(soulContract.address, 1)).to.equal(1);
+
+    expect(await contract.soulOwnerOf(0)).to.equal(guestKey.address);
+    expect(await contract.soulOwnerOf(1)).to.equal(ownerKey.address);
+  });
+
+  it('ownerOf(), soulContracts[], soulTokens[]', async function () {
+    await contract.ownerMint(soulContract.address, 1);
+    await contract.ownerMint(soulContract.address, 0);
+
+    [soulAddr0, soulTid0] = await contract.ownerOf(0);
+    [soulAddr1, soulTid1] = await contract.ownerOf(1);
+    expect(soulAddr0).to.equal(soulContract.address);
+    expect(soulTid0).to.equal(1);
+    expect(soulAddr1).to.equal(soulContract.address);
+    expect(soulTid1).to.equal(0);
+
+    expect(await contract.soulContracts(0)).to.equal(soulContract.address);
+    expect(await contract.soulContracts(1)).to.equal(soulContract.address);
+    expect(await contract.soulTokens(0)).to.equal(1);
+    expect(await contract.soulTokens(1)).to.equal(0);
+  });
+
+  it(`Minting - whitelistMint()`, async function () {
+    await contract.setSigner(signerKey.address);
+
+    const { chainId } = await ethers.provider.getNetwork();
+    let sig = signWhitelist(
+      chainId,
+      contract.address,
+      signerKey,
+      ownerKey.address
+    );
+
+    // verify signature
+    expect(await contract.verifySignature(sig)).to.equal(true);
+    await contract.whitelistMint(sig, soulContract.address, 1);
+
+    expect(await contract.totalSupply()).to.equal(1);
+    expect(await contract.balanceOfSoul(soulContract.address, 0)).to.equal(0);
+    expect(await contract.balanceOfSoul(soulContract.address, 1)).to.equal(1);
+    expect(await contract.balanceOfSoul(soulContract.address, 2)).to.equal(0);
+
+    await expect(
+      contract.whitelistMint(sig, soulContract.address, 2)
+    ).to.be.revertedWith('Caller is not Soul token owner');
+
+    // whitelist mint for guest
+    sig = signWhitelist(chainId, contract.address, signerKey, guestKey.address);
+    await contract
+      .connect(guestKey)
+      .whitelistMint(sig, soulContract.address, 0);
+
+    // whitelist mint for guest2
+    sig = signWhitelist(
+      chainId,
+      contract.address,
+      signerKey,
+      guestKey2.address
+    );
+    await contract
+      .connect(guestKey2)
+      .whitelistMint(sig, soulContract.address, 2);
+
+    expect(await contract.totalSupply()).to.equal(3);
+    expect(await contract.balanceOfSoul(soulContract.address, 0)).to.equal(1);
+    expect(await contract.balanceOfSoul(soulContract.address, 1)).to.equal(1);
+    expect(await contract.balanceOfSoul(soulContract.address, 2)).to.equal(1);
+  });
+});
+
+async function signWhitelist(
+  chainId,
+  contractAddress,
+  whitelistKey,
+  mintingAddress
+) {
+  // Domain data should match whats specified in the DOMAIN_SEPARATOR constructed in the contract
+  // https://github.com/msfeldstein/EIP712-whitelisting/blob/main/contracts/EIP712Whitelisting.sol#L33-L43
+  const domain = {
+    name: 'WhitelistToken',
+    version: '1',
+    chainId,
+    verifyingContract: contractAddress,
+  };
+
+  // The types should match the TYPEHASH specified in the contract
+  // https://github.com/msfeldstein/EIP712-whitelisting/blob/main/contracts/EIP712Whitelisting.sol#L27-L28
+  const types = {
+    Minter: [{ name: 'wallet', type: 'address' }],
+  };
+
+  const sig = await whitelistKey._signTypedData(domain, types, {
+    wallet: mintingAddress,
+  });
+
+  return sig;
+}
